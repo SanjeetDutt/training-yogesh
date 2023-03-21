e24f263e7bb9257c45cdd76c079f3ea5384670ef
SANJEET <sanjeetdutt@gmail.com>
2023-03-21 23:38:03 +0530

    Template 1

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,58 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1>Hello world !! {{name}}</h1>
+    <h2>{{count}}</h2>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <p v-html="text" :class="textClass"></p>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <p v-bind="attrBind">This is a dynamic paragraph</p>
 
-  <RouterView />
+    <div></div>
+
+    <ul>
+        <li v-for="name in names">{{name}}</li>
+    </ul>
 </template>
 
+<script>
+
+export default {
+    name:"mainComp",
+    data(){
+        return {
+            count : 1,
+            name:{
+                fName: "sanjeet",
+                lastName:"Dutt"
+            },
+            names:[
+                "SANJEET","YOGESH","Srinivas"
+            ],
+            text:"<b>THis is error</b>",
+            textClass:"green",
+
+            attrBind:{
+                class:"red",
+                id:"my-dynamic-p",
+                title:"my dynamic paragraph"
+            }
+        }
+    },
+    methods:{
+        click(){
+            alert("HELLO")
+        }
+    }
+
+
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.red{
+    color: red;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.green{
+    color: green;
 }
 </style>
